d6c76dbbd3cf2e270080c697c78a135ce2773d16
K10 <andrewmawson74@gmail.com>
2020-03-11 17:44:29 +0000

    Site Details UI stuff

--- HavenHolidays/Site Details/SiteDetailsView.swift
+++ HavenHolidays/Site Details/SiteDetailsView.swift
@@ -9,16 +9,47 @@
 import UIKit
 
 class SiteDetailsView: UIViewController {
-    //MARK: - Variables
-    var presenter: SiteDetailsPresenterProtocol?
+	//MARK: - Variables
+	var presenter: SiteDetailsPresenterProtocol?
 	var site:SiteViewModel!
-	private var groups:GroupsViewModel?
-    //MARK: - LifeCycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	private var groups:GroupsViewModel? {
+		didSet {
+			configureResults()
+		}
+	}
+	
+	@IBOutlet weak var siteTitleLabel: UILabel!
+	@IBOutlet weak var totalGroupsLabel: UILabel!
+	@IBOutlet weak var groupsSlider: UISlider!
+	@IBOutlet weak var resultsLabel: UILabel!
+	
+	//MARK: - LifeCycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		assert(site != nil, "Site must not be nil")
 		presenter?.interactor?.getGroup(forSite: site)
-    }
+		configureResults()
+	}
+	
+	private func configureResults() {
+		DispatchQueue.main.async {
+			self.siteTitleLabel.text = self.site.title
+			guard let groupsViewModel = self.groups else {
+				return
+			}
+			
+			let groupCount = groupsViewModel.groups.count
+			self.groupsSlider.maximumValue = Float(groupCount)
+			self.groupsSlider.minimumValue = Float(1)
+			self.totalGroupsLabel.text = "Group \(Int(self.groupsSlider.value)) of \(groupCount)"
+			self.resultsLabel.text = ""
+		}
+	}
+	
+	@IBAction func groupSlider(_ sender: UISlider) {
+		print(Int(sender.value))
+		configureResults()
+	}
 }
 
 extension SiteDetailsView {
@@ -28,6 +59,5 @@ extension SiteDetailsView {
 extension SiteDetailsView: SiteDetailsViewProtocol {
 	func display(groups: GroupsViewModel) {
 		self.groups = groups
-		print(self.groups!)
 	}
 }
