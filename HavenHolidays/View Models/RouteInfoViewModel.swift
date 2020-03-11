a1ad17ef2f5901cc5e66bb189339f2c95d2569a7
K10 <andrewmawson74@gmail.com>
2020-03-11 20:21:07 +0000

    Create RouteInfo Object

---
+++ HavenHolidays/View Models/RouteInfoViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RouteInfoViewModel.swift
+//  HavenHolidays
+//
+//  Created by K10 on 11/03/2020.
+//  Copyright © 2020 k10studios. All rights reserved.
+//
+
+import Foundation
+
+struct RouteInfoViewModel {
+	let minutesWait:Int
+	let routeTake:Routes
+}
