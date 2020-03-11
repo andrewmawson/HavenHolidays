9882191f863ae8d2682e9bc023a15aa660d56c91
K10 <andrewmawson74@gmail.com>
2020-03-11 16:10:30 +0000

    Add ViewModels

---
+++ HavenHolidays/View Models/GroupViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  GroupViewModel.swift
+//  HavenHolidays
+//
+//  Created by K10 on 11/03/2020.
+//  Copyright © 2020 k10studios. All rights reserved.
+//
+
+import Foundation
+
+struct GroupViewModel {
+	let familyid:Int
+	let groupsize:Int
+	let caravan:Int
+}
